ef774cbe3e133dfb97c88ce961683a6e2ab95d8c
Andre Skeene <halodre24@gmail.com>
2020-03-05 00:42:30 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -4,10 +4,10 @@ class Anagram
   def initialize(word)
     @word = word
   end
-  #
-  # def match(word_array)
+
+  def match(word_array)
   #   word_array.select do |word|
   #     word.split("").sort == @word.split("").sort
   #   end
-  # end
+  end
 end
