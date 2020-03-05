497a90ae4c4824858ffda5f57e65a2e517a3601e
Andre Skeene <halodre24@gmail.com>
2020-03-05 00:46:19 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -6,8 +6,8 @@ class Anagram
   end
 
   def match(word_array)
-  #   word_array.select do |word|
-  #     word.split("").sort == @word.split("").sort
-  #   end
+    word_array.select do |word|
+      word.split("").sort == @word.split("").sort
+    end
   end
 end
